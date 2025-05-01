1b2c3263a180a9bad7104ee6b39f2d5f7fdb2864
Kirill Cheremushkin <zaguglov2017@yandex.ru>
2025-05-01 14:33:29 +0300

    Добавлена кнопка.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,6 +42,15 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.btnVar4_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(578, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
