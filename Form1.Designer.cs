c1b907c0953749e67c2f02a1f3f0cf227568edb0
Kirill Cheremushkin <zaguglov2017@yandex.ru>
2025-05-01 14:39:37 +0300

    Вернул функционал кнопке.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,12 +44,13 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(578, 172);
+            this.button2.Location = new System.Drawing.Point(631, 172);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(106, 54);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "вариант 5";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
