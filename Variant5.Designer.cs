87a8ff7323d8d56b7c94745127531e7d52b1a76d
Kirill Cheremushkin <zaguglov2017@yandex.ru>
2025-05-01 14:55:37 +0300

    Добавление функционала формы.

--- Variant5.Designer.cs
+++ Variant5.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea9 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend9 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
@@ -53,7 +53,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(393, 714);
+            this.label2.Location = new System.Drawing.Point(410, 714);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(386, 26);
             this.label2.TabIndex = 18;
@@ -63,7 +63,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(393, 676);
+            this.label1.Location = new System.Drawing.Point(410, 676);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(355, 26);
             this.label1.TabIndex = 17;
@@ -71,7 +71,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(384, 645);
+            this.groupBox1.Location = new System.Drawing.Point(401, 645);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(688, 113);
             this.groupBox1.TabIndex = 21;
@@ -82,7 +82,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(384, 509);
+            this.label4.Location = new System.Drawing.Point(401, 509);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(338, 26);
             this.label4.TabIndex = 20;
@@ -92,7 +92,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(384, 468);
+            this.label3.Location = new System.Drawing.Point(401, 468);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(34, 26);
             this.label3.TabIndex = 19;
@@ -100,23 +100,24 @@
             // 
             // buttonForecast
             // 
-            this.buttonForecast.Location = new System.Drawing.Point(384, 553);
+            this.buttonForecast.Location = new System.Drawing.Point(401, 553);
             this.buttonForecast.Name = "buttonForecast";
             this.buttonForecast.Size = new System.Drawing.Size(504, 50);
             this.buttonForecast.TabIndex = 16;
             this.buttonForecast.Text = "Сделать прогноз";
             this.buttonForecast.UseVisualStyleBackColor = true;
+            this.buttonForecast.Click += new System.EventHandler(this.buttonForecast_Click);
             // 
             // numericUpDownForecastYears
             // 
-            this.numericUpDownForecastYears.Location = new System.Drawing.Point(768, 513);
+            this.numericUpDownForecastYears.Location = new System.Drawing.Point(785, 513);
             this.numericUpDownForecastYears.Name = "numericUpDownForecastYears";
             this.numericUpDownForecastYears.Size = new System.Drawing.Size(120, 22);
             this.numericUpDownForecastYears.TabIndex = 15;
             // 
             // numericUpDownN
             // 
-            this.numericUpDownN.Location = new System.Drawing.Point(424, 473);
+            this.numericUpDownN.Location = new System.Drawing.Point(441, 473);
             this.numericUpDownN.Name = "numericUpDownN";
             this.numericUpDownN.Size = new System.Drawing.Size(120, 22);
             this.numericUpDownN.TabIndex = 14;
@@ -128,7 +129,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(324, 291);
+            this.dataGridView1.Size = new System.Drawing.Size(357, 291);
             this.dataGridView1.TabIndex = 13;
             // 
             // buttonLoadData
@@ -148,19 +149,20 @@
             this.button1.TabIndex = 22;
             this.button1.Text = "Загрузить данные";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea9.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea9);
+            legend9.Name = "Legend1";
+            this.chart1.Legends.Add(legend9);
             this.chart1.Location = new System.Drawing.Point(27, 68);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series9.ChartArea = "ChartArea1";
+            series9.Legend = "Legend1";
+            series9.Name = "Series1";
+            this.chart1.Series.Add(series9);
             this.chart1.Size = new System.Drawing.Size(1100, 382);
             this.chart1.TabIndex = 23;
             this.chart1.Text = "chart1";
